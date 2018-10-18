920ebedd8d3868280d2eba90e4babe44a0e4bc46
KeenKing <969172689@qq.com>
2018-10-18 22:12:05 +0800

    修改了footer的bug

---
+++ 租赁网站/js/index.js
@@ -0,0 +1,12 @@
+//js控制轮播图
+$(document).ready(function (e) {
+  var unslider04 = $('#b04').unslider({
+      dots: true
+    }),
+    data04 = unslider04.data('unslider');
+
+  $('.unslider-arrow04').click(function () {
+    var fn = this.className.split(' ')[1];
+    data04[fn]();
+  });
+});
