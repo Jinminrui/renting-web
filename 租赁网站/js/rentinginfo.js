0b7a3498f7b1cda895207ccec94b522bfafc3327
KeenKing <969172689@qq.com>
2018-10-23 23:40:04 +0800

    10.23 23：40

---
+++ 租赁网站/js/rentinginfo.js
@@ -0,0 +1,13 @@
+/**
+ * 跳转到完整的求租信息界面
+ */
+function ToSpecificRenting() {
+  window.open('specificrentinginfo.html');
+}
+
+/**
+* 跳转到发布求租消息界面
+*/
+function TopostRentinginfo() {
+  window.open('postrentinginfo.html');
+}
